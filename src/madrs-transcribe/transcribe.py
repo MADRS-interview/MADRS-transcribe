f36e01af863d21fd65b54d309e6a4cdabd693b50
Kerry Cerqueira <kerrycerqueira@gmail.com>
2024-11-14 13:27:35 -0500

    Add batch-transcribe notebook and update transcribe module

--- src/madrs-transcribe/transcribe.py
+++ src/madrs-transcribe/transcribe.py
@@ -1,69 +1,65 @@
 from pyannote.audio import Pipeline as Pyannote_Pipeline
-from pyannote.audio.pipelines.utils.hook import ProgressHook
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline as Transformers_Pipeline
 
 import librosa
 import torch
 
 def Pipeline(
-    auth_token,
     diarization_model="pyannote/speaker-diarization-3.1",
-    transcription_model = "openai/whisper-tiny",
-    pad=0.5,
-    sample_rate = 16000,
-    collar = 3,
+    transcription_model = "openai/whisper-large-v3",
+    collar = 2,
+    diarize = True,
 ):
     return lambda x: PipelineGenerator(
-        auth_token,
-        diarization_model,
-        transcription_model,
-        pad,
-        sample_rate,
-        collar,
+        diarization_model = diarization_model,
+        transcription_model = transcription_model,
+        collar = collar,
+        diarize = diarize,
     ).transcribe(x)
 
 class PipelineGenerator:
 
     def __init__(
         self,
-        auth_token,
+        *,
         diarization_model,
         transcription_model,
-        pad,
-        sample_rate,
         collar,
+        diarize,
     ):
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        device = "cuda" if torch.cuda.is_available() else "cpu"
         self.diarization_pipeline = Pyannote_Pipeline.from_pretrained(
             diarization_model,
-            use_auth_token = auth_token,
         ).to(torch.device(device))
-        processor = AutoProcessor.from_pretrained(transcription_model)
         self.transcription_pipeline = Transformers_Pipeline(
             "automatic-speech-recognition",
-            model = AutoModelForSpeechSeq2Seq.from_pretrained(
-                transcription_model,
-                torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32,
-                low_cpu_mem_usage=True,
-                use_safetensors=True
-            ).to(device),
-            tokenizer= processor.tokenizer,
-            feature_extractor= processor.feature_extractor,
+            model = transcription_model,
             return_timestamps=True,
             generate_kwargs={"language": "english"},
+            device = device,
         )
-        self.pad = pad
-        self.sample_rate = sample_rate
+        self.pad = 0.5
+        self.sample_rate = 16000
         self.collar = collar
+        self.num_speakers = 2
+        self.diarize = diarize
 
     def transcribe(self, target):
-        with ProgressHook() as hook:
-            diarization = (
-                self
-                .diarization_pipeline(target, hook=hook)
-                .support(collar = self.collar)
+        if self.diarize:
+            return self.diarize_transcribe(target)
+        else:
+            return self.nodiarize_transcribe(target)
+
+    def diarize_transcribe(self, target):
+        diarization = (
+            self
+            .diarization_pipeline(
+                target, 
+                num_speakers = self.num_speakers
             )
-        audio, _ = librosa.load(target, sr=self.sample_rate)
+            .support(collar = self.collar)
+        )
+        audio, _ = librosa.load(target, sr = self.sample_rate)
         transcript = ""
         for segment, _, label in diarization.itertracks(yield_label=True):
             audio_segment = audio[
@@ -73,3 +69,8 @@ class PipelineGenerator:
             transcript_segment = self.transcription_pipeline(audio_segment)
             transcript += label + ":" + transcript_segment["text"] + "\n\n"
         return transcript
+
+    def nodiarize_transcribe(self, target):
+        audio, _ = librosa.load(target, sr=self.sample_rate)
+        return self.transcription_pipeline(audio)["text"]
+            
